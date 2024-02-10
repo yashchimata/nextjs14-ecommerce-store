bc2edc08ee7315ec04a3053e862013fc0b16f3fe
Yashwanth Chimata <ch.yashwanth1999@gmail.com>
2024-02-10 16:38:49 -0600

    modified:   components/gallery/index.tsx 	modified:   package-lock.json 	modified:   package.json

--- components/gallery/index.tsx
+++ components/gallery/index.tsx
@@ -14,7 +14,7 @@ interface GalleryProps {
 const Gallery: React.FC<GalleryProps> = ({
   images = []
 }) => {
-  return ( 
+  return (
     <Tab.Group as="div" className="flex flex-col-reverse">
       <div className="mx-auto mt-6 hidden w-full max-w-2xl sm:block lg:max-w-none">
         <Tab.List className="grid grid-cols-4 gap-6">
